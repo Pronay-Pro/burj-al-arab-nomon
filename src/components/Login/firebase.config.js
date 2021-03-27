31d718e8f4a381dadd60be2e3c9e58f46fea386a
Pronay Saha <pronaysaha788@gmail.com>
2021-03-27 06:44:10 +0600

    burj-al-arab

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ0vK_3dU4BvgCyF6YV7OayakhWxh65_o",
+  authDomain: "burja-al-arab.firebaseapp.com",
+  projectId: "burja-al-arab",
+  storageBucket: "burja-al-arab.appspot.com",
+  messagingSenderId: "9089898509",
+  appId: "1:9089898509:web:349d439040f64d515a9d44"
+  };
+  export default firebaseConfig;
